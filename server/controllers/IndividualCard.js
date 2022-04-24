ab3c3a1f3d2e91f542d8c9f408edb8a3c12a4731
Melody Davis <piano.fan122@gmail.com>
2022-04-24 18:06:42 -0400

    get individual cards

---
+++ server/controllers/IndividualCard.js
@@ -0,0 +1,9 @@
+const Card = require('../models/card')
+
+const getIndividualCard = async (req, res) => {
+    const { id } = req.params
+    const card = await Card.findById(id)
+    res.status(200).json(card)
+}
+
+module.exports = { getIndividualCard }
--- server/routes/cards.js
+++ server/routes/cards.js
@@ -5,10 +5,12 @@ const { getArtemCards } = require('../controllers/ArtemCards')
 const { getLukeCards } = require('../controllers/LukeCards')
 const { getMariusCards } = require('../controllers/MariusCards')
 const { getVynCards } = require('../controllers/VynCards')
+const { getIndividualCard } = require('../controllers/IndividualCard')
 
 router.route('/artem').get(getArtemCards)
 router.route('/luke').get(getLukeCards)
 router.route('/marius').get(getMariusCards)
 router.route('/vyn').get(getVynCards)
+router.route('/:id').get(getIndividualCard)
 
 module.exports = router
